48c240c30363aef2ec2b370e95a1564ae12fe4da
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-09 12:24:42 +0530

    route-tables-creation and association-using terraform

--- resources/main.tf
+++ resources/main.tf
@@ -32,3 +32,31 @@ resource "aws_internet_gateway" "igw-example-instance" {
     Name = "igw-example-instance"
   }
 }
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.use-this-vpc-for-example-instance.id
+
+  route {
+    cidr_block = "10.40.1.0/24"
+    gateway_id = aws_internet_gateway.igw-example-instance.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route" "add-routes-public-route-table" {
+  route_table_id            = aws_route_table.public-route-table.id
+  destination_cidr_block    = "10.40.1.0/24"
+}
+
+resource "aws_route_table_association" "subnet-association" {
+  subnet_id      = aws_subnet.use-this-vpc-subnet-for-example-instance.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+resource "aws_route_table_association" "igw-association" {
+  gateway_id     = aws_internet_gateway.igw-example-instance.id
+  route_table_id = aws_route_table.public-route-table.id
+}
