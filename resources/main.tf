c5a6f80a16717000d76a49e56feacecbd5d3f797
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-09 12:02:08 +0530

    instance-creation-using -terraform

--- resources/main.tf
+++ resources/main.tf
@@ -6,3 +6,29 @@ resource "aws_instance" "example-instance" {
     Name = "Example-instance"
   }
 }
+
+resource "aws_vpc" "use-this-vpc-for-example-instance" {
+  cidr_block       = "10.40.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "use-this-vpc-for-example-instance"
+  }
+}
+
+resource "aws_subnet" "use-this-vpc-subnet-for-example-instance" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.40.1.0/24"
+
+  tags = {
+    Name = "use-this-vpc-subnet-for-example-instance"
+  }
+}
+
+resource "aws_internet_gateway" "igw-example-instance" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "igw-example-instance"
+  }
+}
