71db57ca8346dcab534bed020fde97fe90f2d87f
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-09 12:52:13 +0530

    aws -route-table-creation and association using terraform

--- resources/main.tf
+++ resources/main.tf
@@ -36,26 +36,26 @@ resource "aws_internet_gateway" "igw-example-instance" {
 resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.use-this-vpc-for-example-instance.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw-example-instance.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.igw-example-instance.id
+#   }
 
   tags = {
     Name = "public-route-table"
   }
 }
 
-# resource "aws_route" "add-routes-public-route-table" {
-#   route_table_id            = aws_route_table.public-route-table.id
-#   destination_cidr_block    = "0.0.0.0/0"
-#   gateway_id = aws_internet_gateway.igw-example-instance.id
-# }
-
-resource "aws_route_table_association" "subnet-association" {
-  subnet_id      = aws_subnet.use-this-vpc-subnet-for-example-instance.id
-  route_table_id = aws_route_table.public-route-table.id
+resource "aws_route" "add-routes-public-route-table" {
+  route_table_id            = aws_route_table.public-route-table.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw-example-instance.id
 }
+
+# resource "aws_route_table_association" "subnet-association" {
+#   subnet_id      = aws_subnet.use-this-vpc-subnet-for-example-instance.id
+#   route_table_id = aws_route_table.public-route-table.id
+# }
 
 resource "aws_route_table_association" "igw-association" {
   gateway_id     = aws_internet_gateway.igw-example-instance.id
