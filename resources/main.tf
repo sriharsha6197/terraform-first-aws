9eed42a5f922e539b78ef6ba7225489e79e9ad80
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-15 22:58:54 +0530

    variables decalaration and printing outputs

--- resources/main.tf
+++ resources/main.tf
@@ -123,3 +123,30 @@ resource "aws_route53_record" "backend" {
   ttl = 300
   records = [aws_instance.backend_terraform.private_ip]
 }
+
+variable "fruit" {
+  type = string
+  description = "variable name has a fruit value"
+  default = "apple"
+}
+variable "fruits" {
+  type = list(string)
+  default = ["apple","banana"]
+}
+variable "fruit_price" {
+  type = map(string)
+  default = {
+    apple = 100
+    banana = 20
+  }
+}
+
+output "fruit_name" {
+  value = var.fruit
+}
+output "first_fruit" {
+  value = var.fruits[0]
+}
+output "fruit_price" {
+  value = "the price of ${var.fruits[0]} is ${var.fruit_price["apple"]}"
+}
