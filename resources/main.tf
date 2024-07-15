3e794b83252923166a30235a73bdc7f715c9da1c
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-15 16:32:07 +0530

    route table

--- resources/main.tf
+++ resources/main.tf
@@ -27,7 +27,7 @@ resource "aws_route_table" "PublicRouteTableTerraform" {
   vpc_id = aws_vpc.vpcEC2Terraform.id
 
   route {
-    cidr_block = "192.168.1.0/24"
+    cidr_block = "192.168.0.0/16"
     gateway_id = aws_internet_gateway.IGWTerraform.id
   }
 
