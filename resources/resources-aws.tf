1a0672e9d575c4afafdf001d148c7e8c7690251d
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-17 16:26:58 +0530

    frontend, mysql, backend servers of null resource

--- resources/resources-aws.tf
+++ resources/resources-aws.tf
@@ -28,7 +28,7 @@ resource "null_resource" "frontend_setup" {
     dnf list all | grep python
     dnf install python3.12-pip.noarch -y
     pip3.12 install botocore boto3
-    ansible-pull -i ${self.private_ip}, -U https://github.com/sriharsha6197/expense-ansible-test.git -e ansible_user=centos -e ansible_password=DevOps321 expense.yaml -e role_name=frontend
+    ansible-pull -i ${aws_instance.frontend_instance_terraform.private_ip}, -U https://github.com/sriharsha6197/expense-ansible-test.git -e ansible_user=centos -e ansible_password=DevOps321 expense.yaml -e role_name=frontend
     EOF
   }
 }
@@ -60,7 +60,7 @@ resource "null_resource" "mysql_setup" {
     dnf install ansible -y
     dnf install python3.12-pip.noarch -y
     pip3.12 install botocore boto3
-    ansible-pull -i ${self.private_ip}, -U https://github.com/sriharsha6197/expense-ansible-test.git -e ansible_user=centos -e ansible_password=DevOps321 expense.yaml -e role_name=mysql
+    ansible-pull -i ${aws_instance.mysql_terraform.private_ip}, -U https://github.com/sriharsha6197/expense-ansible-test.git -e ansible_user=centos -e ansible_password=DevOps321 expense.yaml -e role_name=mysql
     EOF
   }
 }
@@ -91,7 +91,7 @@ resource "null_resource" "backend" {
     dnf install ansible -y
     dnf install python3.12-pip.noarch -y
     pip3.12 install botocore boto3
-    ansible-pull -i ${self.private_ip}, -U https://github.com/sriharsha6197/expense-ansible-test.git -e ansible_user=centos -e ansible_password=DevOps321 expense.yaml -e role_name=backend
+    ansible-pull -i ${aws_instance.backend_terraform.private_ip}, -U https://github.com/sriharsha6197/expense-ansible-test.git -e ansible_user=centos -e ansible_password=DevOps321 expense.yaml -e role_name=backend
     EOF
   }
 }
