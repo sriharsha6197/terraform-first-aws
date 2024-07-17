146c62e63e4137e3b656c39dec83dd829eb40c53
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-17 12:55:59 +0530

    commenting data blocks

--- resources/data.tf
+++ resources/data.tf
@@ -1,31 +1,31 @@
-##################################DATA-BLOCK-FOR-AWS_AMI#############
-data "aws_ami" "centos_ami" {
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = [973714476881]
-}
+# ##################################DATA-BLOCK-FOR-AWS_AMI#############
+# data "aws_ami" "centos_ami" {
+#   most_recent      = true
+#   name_regex       = "Centos-8-DevOps-Practice"
+#   owners           = [973714476881]
+# }
 
-###################################VARIABLE_AND_DATA_BLOCK_FOR_SUBNETiD#########
-variable "subnet_id" {
-  default = "subnet-09fab819bd4be3d64"
-}
-data "aws_subnet" "datablock_subnet" {
-  id = var.subnet_id
-}
+# ###################################VARIABLE_AND_DATA_BLOCK_FOR_SUBNETiD#########
+# variable "subnet_id" {
+#   default = "subnet-09fab819bd4be3d64"
+# }
+# data "aws_subnet" "datablock_subnet" {
+#   id = var.subnet_id
+# }
 
-##################################SECURITYGROUP_VARIABLE_BLOCK_DATA_BLOCK#######
-variable "security_group_id" {
-  default = "sg-0d5c35489f4c8a206"
-}
-data "aws_security_group" "datablock_security_group" {
-  depends_on = [ aws_security_group.allow_all_traffic_terraform.id ]
-  id = var.security_group_id
-}
+# ##################################SECURITYGROUP_VARIABLE_BLOCK_DATA_BLOCK#######
+# variable "security_group_id" {
+#   default = "sg-0d5c35489f4c8a206"
+# }
+# data "aws_security_group" "datablock_security_group" {
+#   depends_on = [ aws_security_group.allow_all_traffic_terraform.id ]
+#   id = var.security_group_id
+# }
 
-#####################################ROUTE53_ZONE_ID###############
-variable "hosted_zone" {
-  default = "sriharsha.shop"
-}
-data "aws_route53_zone" "hosted_zone" {
-  name = var.hosted_zone
-}
+# #####################################ROUTE53_ZONE_ID###############
+# variable "hosted_zone" {
+#   default = "sriharsha.shop"
+# }
+# data "aws_route53_zone" "hosted_zone" {
+#   name = var.hosted_zone
+# }
