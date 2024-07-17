7c7944ea8220b4a9d27bb6a3917d07bf024a1073
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-17 20:42:16 +0530

    instance2

--- resources/resources-aws.tf
+++ resources/resources-aws.tf
@@ -133,7 +133,39 @@ resource "aws_instance" "testing_instance_provisioner_local_exec" {
   }
 }
 
-resource "null_resource" "testing_instance_provisioner_local_exec_test" {
+resource "null_resource" "testing_instance_provisioner_local_exec_test2" {
+  triggers = {
+    instance_id = aws_instance.testing_instance_provisioner_local_exec.id
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+    dnf install nginx -y
+    systemctl enable nginx
+    systemctl start nginx
+    EOF
+  }
+}
+
+
+
+resource "aws_instance" "testing_instance2_provisioner_local_exec" {
+  ami = data.aws_ami.centos_ami.id
+  instance_type = "t2.micro"
+  subnet_id = data.aws_subnet.datablock_subnet.id
+  vpc_security_group_ids = [data.aws_security_group.datablock_security_group.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "backend_terraform"
+  }
+}
+
+resource "null_resource" "testing_instance_provisioner_local_exec_test2" {
+  triggers = {
+    instance_id = aws_instance.testing_instance2_provisioner_local_exec.id
+  }
+
   provisioner "local-exec" {
     command = <<EOF
     dnf install nginx -y
