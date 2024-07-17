1d118f9b276713a7cdc24293d8e1a908f6980377
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-17 15:32:58 +0530

    commenting out backend and mysql servers

--- resources/resources-aws.tf
+++ resources/resources-aws.tf
@@ -1,36 +1,36 @@
-# ###############################FRONTEND_BACKEND_MYSQL_DNSRECORDSOF_FRONTEND_MYSQL_BACKEND##############
-# resource "aws_instance" "frontend_instance_terraform" {
-#   ami           = data.aws_ami.centos_ami.image_id
-#   instance_type = "t2.micro"
-#   subnet_id = data.aws_subnet.datablock_subnet.id
-#   vpc_security_group_ids = [data.aws_security_group.datablock_security_group.id]
-#   associate_public_ip_address = true
+###############################FRONTEND_BACKEND_MYSQL_DNSRECORDSOF_FRONTEND_MYSQL_BACKEND##############
+resource "aws_instance" "frontend_instance_terraform" {
+  ami           = data.aws_ami.centos_ami.image_id
+  instance_type = "t2.micro"
+  subnet_id = data.aws_subnet.datablock_subnet.id
+  vpc_security_group_ids = [data.aws_security_group.datablock_security_group.id]
+  associate_public_ip_address = true
 
-#   tags = {
-#     Name = "frontendTerraform"
-#   }
-# }
-# resource "aws_route53_record" "www" {
-#   allow_overwrite = true
-#   zone_id = data.aws_route53_zone.hosted_zone.zone_id
-#   name    = "frontend.${var.hosted_zone}"
-#   type    = "A"
-#   ttl     = 300
-#   records = [aws_instance.frontend_instance_terraform.private_ip]
-# }
+  tags = {
+    Name = "frontendTerraform"
+  }
+}
+resource "aws_route53_record" "www" {
+  allow_overwrite = true
+  zone_id = data.aws_route53_zone.hosted_zone.zone_id
+  name    = "frontend.${var.hosted_zone}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend_instance_terraform.private_ip]
+}
 
-# resource "null_resource" "frontend_setup" {
-#   provisioner "local-exec" {
-#     command = <<EOF
-#     pwd
-#     dnf install ansible -y
-#     dnf list all | grep python
-#     dnf install python3.12-pip.noarch -y
-#     pip3.12 install botocore boto3
-#     ansible-pull -i localhost, -U https://github.com/sriharsha6197/expense-ansible-test.git -e ansible_user=centos -e ansible_password=DevOps321 expense.yaml -e role_name=frontend
-#     EOF
-#   }
-# }
+resource "null_resource" "frontend_setup" {
+  provisioner "local-exec" {
+    command = <<EOF
+    pwd
+    dnf install ansible -y
+    dnf list all | grep python
+    dnf install python3.12-pip.noarch -y
+    pip3.12 install botocore boto3
+    ansible-pull -i localhost, -U https://github.com/sriharsha6197/expense-ansible-test.git -e ansible_user=centos -e ansible_password=DevOps321 expense.yaml -e role_name=frontend
+    EOF
+  }
+}
 
 # resource "aws_instance" "mysql_terraform" {
 #   ami = data.aws_ami.centos_ami.image_id
