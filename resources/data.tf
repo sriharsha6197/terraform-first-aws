d7f9650d03a893fea53240353cc094f58e28760d
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-18 17:38:51 +0530

    uncommenting the data and resources files

--- resources/data.tf
+++ resources/data.tf
@@ -1,30 +1,30 @@
-# ##################################DATA-BLOCK-FOR-AWS_AMI#############
-# data "aws_ami" "centos_ami" {
-#   most_recent      = true
-#   name_regex       = "Centos-8-DevOps-Practice"
-#   owners           = [973714476881]
-# }
+##################################DATA-BLOCK-FOR-AWS_AMI#############
+data "aws_ami" "centos_ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = [973714476881]
+}
 
-# ###################################VARIABLE_AND_DATA_BLOCK_FOR_SUBNETiD#########
-# variable "subnet_id" {
-#   default = "subnet-08f4b927691b9f798"
-# }
-# data "aws_subnet" "datablock_subnet" {
-#   id = var.subnet_id
-# }
+###################################VARIABLE_AND_DATA_BLOCK_FOR_SUBNETiD#########
+variable "subnet_id" {
+  default = "subnet-0b3ffc9e5da94a034"
+}
+data "aws_subnet" "datablock_subnet" {
+  id = var.subnet_id
+}
 
-# ##################################SECURITYGROUP_VARIABLE_BLOCK_DATA_BLOCK#######
-# variable "security_group_id" {
-#   default = "sg-0d841a452b8940f64"
-# }
-# data "aws_security_group" "datablock_security_group" {
-#   id = var.security_group_id
-# }
+##################################SECURITYGROUP_VARIABLE_BLOCK_DATA_BLOCK#######
+variable "security_group_id" {
+  default = "sg-081d5075e31848bef"
+}
+data "aws_security_group" "datablock_security_group" {
+  id = var.security_group_id
+}
 
-# #####################################ROUTE53_ZONE_ID###############
-# variable "hosted_zone" {
-#   default = "sriharsha.shop"
-# }
-# data "aws_route53_zone" "hosted_zone" {
-#   name = var.hosted_zone
-# }
+#####################################ROUTE53_ZONE_ID###############
+variable "hosted_zone" {
+  default = "sriharsha.shop"
+}
+data "aws_route53_zone" "hosted_zone" {
+  name = var.hosted_zone
+}
