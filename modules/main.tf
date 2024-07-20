b3040acb86f80b2add14d7ad67e3e860a335748d
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-20 19:02:32 +0530

    config

--- modules/main.tf
+++ modules/main.tf
@@ -1,4 +1,15 @@
 module "frontend" {
   source = "./expense-app-create"
+  component = "frontend"
+}
+
+module "mysql" {
+  source = "./expense-app-create"
+  component = "mysql"
+}
+
+module "backend" {
+  source = "./expense-app-create"
+  component = "backend"
 }
 
