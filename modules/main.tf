66092a6ed6346cdefbe664619960991d7ed896eb
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-21 16:13:31 +0530

    moving vpx resource file from child module into root module

--- modules/main.tf
+++ modules/main.tf
@@ -1,7 +1,6 @@
 module "expense" {
   count = length(var.instances)
   source = "./expense-app-create"
-  for_each = [aws_vpc.vpcEC2Terraform, aws_subnet.vpcEC2TerraformSubnet, aws_internet_gateway.IGWTerraform, aws_route_table.PublicRouteTableTerraform, aws_security_group.allow_all_traffic_terraform]
   component = var.instances[count.index]
 }
 
