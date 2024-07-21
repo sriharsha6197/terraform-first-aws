9c3235fb523fff8921da21cd289a9822cb750bee
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-21 16:45:05 +0530

    creating instances

--- modules/expense-app-create/data.tf
+++ modules/expense-app-create/data.tf
@@ -1,44 +1,44 @@
-# ##################################DATA-BLOCK-FOR-AWS_AMI#############
-# data "aws_ami" "centos_ami" {
-#   most_recent      = true
-#   name_regex       = "Centos-8-DevOps-Practice"
-#   owners           = [973714476881]
-# }
+##################################DATA-BLOCK-FOR-AWS_AMI#############
+data "aws_ami" "centos_ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = [973714476881]
+}
 
-# ###################################VARIABLE_AND_DATA_BLOCK_FOR_SUBNETiD#########
-# variable "subnet_id" {
-#   default = "subnet-0a57473e438a875c7"
-# }
-# data "aws_subnet" "datablock_subnet" {
-#   id = var.subnet_id
-# }
+###################################VARIABLE_AND_DATA_BLOCK_FOR_SUBNETiD#########
+variable "subnet_id" {
+  default = "subnet-0a57473e438a875c7"
+}
+data "aws_subnet" "datablock_subnet" {
+  id = var.subnet_id
+}
 
-# ##################################SECURITYGROUP_VARIABLE_BLOCK_DATA_BLOCK#######
-# variable "security_group_id" {
-#   default = "sg-0b4d326a5b20d3639"
-# }
-# data "aws_security_group" "datablock_security_group" {
-#   id = var.security_group_id
-# }
+##################################SECURITYGROUP_VARIABLE_BLOCK_DATA_BLOCK#######
+variable "security_group_id" {
+  default = "sg-0b4d326a5b20d3639"
+}
+data "aws_security_group" "datablock_security_group" {
+  id = var.security_group_id
+}
 
-# #####################################ROUTE53_ZONE_ID###############
-# variable "hosted_zone" {
-#   default = "sriharsha.shop"
-# }
-# data "aws_route53_zone" "hosted_zone" {
-#   name = var.hosted_zone
-# }
+#####################################ROUTE53_ZONE_ID###############
+variable "hosted_zone" {
+  default = "sriharsha.shop"
+}
+data "aws_route53_zone" "hosted_zone" {
+  name = var.hosted_zone
+}
 
-# ###############################Local_block_and_variables##############
-# locals {
-#   ami = data.aws_ami.centos_ami.image_id
-#   subnet_id = data.aws_subnet.datablock_subnet.id
-#   security_group_id = data.aws_security_group.datablock_security_group.id
-#   zone_id = data.aws_route53_zone.hosted_zone.zone_id
-# }
+###############################Local_block_and_variables##############
+locals {
+  ami = data.aws_ami.centos_ami.image_id
+  subnet_id = data.aws_subnet.datablock_subnet.id
+  security_group_id = data.aws_security_group.datablock_security_group.id
+  zone_id = data.aws_route53_zone.hosted_zone.zone_id
+}
 
 
-# data "aws_iam_instance_profile" "attach_instance" {
-#   name = "attach_instance"
-#   role_name = "ec2_role_for_instances1"
-# }
+data "aws_iam_instance_profile" "attach_instance" {
+  name = "attach_instance"
+  role_name = "ec2_role_for_instances"
+}
--- modules/expense-app-create/main.tf
+++ modules/expense-app-create/main.tf
@@ -1,45 +1,45 @@
 
-# # ###############################FRONTEND_BACKEND_MYSQL_DNSRECORDSOF_FRONTEND_MYSQL_BACKEND##############
-# resource "aws_instance" "instance" {
-#   ami           = local.ami
-#   instance_type = "t2.micro"
-#   iam_instance_profile = aws_iam_instance_profile.attach_instance.name
-#   subnet_id = local.subnet_id
-#   vpc_security_group_ids = [local.security_group_id]
-#   associate_public_ip_address = true
+# ###############################FRONTEND_BACKEND_MYSQL_DNSRECORDSOF_FRONTEND_MYSQL_BACKEND##############
+resource "aws_instance" "instance" {
+  ami           = local.ami
+  instance_type = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.attach_instance.name
+  subnet_id = local.subnet_id
+  vpc_security_group_ids = [local.security_group_id]
+  associate_public_ip_address = true
 
-#   tags = {
-#     Name = "${var.component}_instance_terraform"
-#   }
+  tags = {
+    Name = "${var.component}_instance_terraform"
+  }
+}
+resource "aws_route53_record" "record" {
+  allow_overwrite = true
+  zone_id = local.zone_id
+  name    = "${var.component}.${var.hosted_zone}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.instance.private_ip]
+}
+# resource "aws_ssm_parameter" "expense_frontend_backend_ip" {
+#   depends_on = [ aws_instance.backend_terraform ]
+#   name  = "expense_frontend_backend_ip"
+#   type  = "String"
+#   value = aws_instance.backend_terraform.private_ip
 # }
-# resource "aws_route53_record" "record" {
-#   allow_overwrite = true
-#   zone_id = local.zone_id
-#   name    = "${var.component}.${var.hosted_zone}"
-#   type    = "A"
-#   ttl     = 300
-#   records = [aws_instance.instance.private_ip]
-# }
-# # resource "aws_ssm_parameter" "expense_frontend_backend_ip" {
-# #   depends_on = [ aws_instance.backend_terraform ]
-# #   name  = "expense_frontend_backend_ip"
-# #   type  = "String"
-# #   value = aws_instance.backend_terraform.private_ip
-# # }
-# resource "null_resource" "frontend_setup" {
-#   connection {
-#     type = "ssh"
-#     user = "centos"
-#     password = "DevOps321"
-#     host = aws_instance.instance.public_ip
-#   }
-#   depends_on = [ aws_route53_record.record ]
-#   provisioner "remote-exec" {
-#     inline = [ 
-#       "sudo dnf install ansible -y",
-#       "sudo dnf install python3.12-pip.noarch -y",
-#       "sudo pip3.12 install botocore boto3",
-#       "sudo ansible-pull -i localhost, -U https://github.com/sriharsha6197/expense-ansible-test.git -e ansible_user=centos -e ansible_password=DevOps321 expense.yaml -e role_name=${var.component}"
-#      ]
-#   }
-# }
+resource "null_resource" "frontend_setup" {
+  connection {
+    type = "ssh"
+    user = "centos"
+    password = "DevOps321"
+    host = aws_instance.instance.public_ip
+  }
+  depends_on = [ aws_route53_record.record ]
+  provisioner "remote-exec" {
+    inline = [ 
+      "sudo dnf install ansible -y",
+      "sudo dnf install python3.12-pip.noarch -y",
+      "sudo pip3.12 install botocore boto3",
+      "sudo ansible-pull -i localhost, -U https://github.com/sriharsha6197/expense-ansible-test.git -e ansible_user=centos -e ansible_password=DevOps321 expense.yaml -e role_name=${var.component}"
+     ]
+  }
+}
