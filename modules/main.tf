8459ab9642670ca64ab2cbe32930f99175d817e0
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-23 13:03:12 +0530

    calling s3 child module from the root module of terraform

--- modules/main.tf
+++ modules/main.tf
@@ -1,14 +1,18 @@
-module "role" {
-  source = "./iam_role"
-}
+# module "role" {
+#   source = "./iam_role"
+# }
 
-module "frontend" {
-  count = length(var.instances)
-  source = "./expense-app-create"
-  component = var.instances[count.index]
-  instance_profile = module.role.instance_role_profile
-}
+# module "frontend" {
+#   count = length(var.instances)
+#   source = "./expense-app-create"
+#   component = var.instances[count.index]
+#   instance_profile = module.role.instance_role_profile
+# }
 
-variable "instances" {
-  default = [ "frontend","backend","mysql" ]
+# variable "instances" {
+#   default = [ "frontend","backend","mysql" ]
+# }
+
+module "s3" {
+  source = "./s3-bucket"
 }
