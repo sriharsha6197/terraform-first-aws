bf0dc7426d607b3b2aca12516e91fe34f85b2543
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-23 14:31:15 +0530

    creating new bucket

--- modules/s3-bucket/main.tf
+++ modules/s3-bucket/main.tf
@@ -2,15 +2,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "my-tf-test-bucket" {
-  bucket = "my-tf-test-bucket"
+resource "aws_s3_bucket" "my-tf-test-bucket-harsha" {
+  bucket = "my-tf-test-bucket-harsha"
 }
 resource "aws_s3_bucket_acl" "acl_bucket" {
-  bucket = aws_s3_bucket.my-tf-test-bucket.id
+  bucket = aws_s3_bucket.my-tf-test-bucket-harsha.id
   acl    = "private"
 }
 resource "aws_s3_bucket_public_access_block" "block_public_access" {
-  bucket = aws_s3_bucket.my-tf-test-bucket.id
+  bucket = aws_s3_bucket.my-tf-test-bucket-harsha.id
 
   block_public_acls       = true
   block_public_policy     = true
@@ -18,7 +18,7 @@ resource "aws_s3_bucket_public_access_block" "block_public_access" {
   restrict_public_buckets = true
 }
 resource "aws_s3_bucket_versioning" "versioning_bucket" {
-  bucket = aws_s3_bucket.my-tf-test-bucket.id
+  bucket = aws_s3_bucket.my-tf-test-bucket-harsha.id
   versioning_configuration {
     status = "Enabled"
   }
