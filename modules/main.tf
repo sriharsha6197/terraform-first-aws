ff576d525a85d96b2683283dbbd216783dd0e232
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-23 18:17:25 +0530

    s3 bucket creation through terraform

--- modules/main.tf
+++ modules/main.tf
@@ -12,10 +12,10 @@
 # variable "instances" {
 #   default = [ "frontend","backend","mysql" ]
 # }
+module "bucket" {
+  source = "./s3-bucket"
+}
 terraform {
-  backend "s3" {
-    bucket = "sri6197-bucket"
-    key = "state-file/terraform.tfstate"
-    region = "us-east-1"
+  backend "local" {
   }
 }
