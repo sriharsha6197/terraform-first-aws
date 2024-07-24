c621e4a8902383e995e07a77af73c21d2f7895f6
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-24 23:08:42 +0530

    new bucket

--- modules/s3-bucket/vars.tf
+++ modules/s3-bucket/vars.tf
@@ -1,4 +1,4 @@
 variable "buckets_to_create" {
   type = set(string)
-  default = ["sri6197-bucket","harsha-7916-bucket","multi-env-bucket"]
+  default = ["sri6197-bucket","harsha-7916-bucket","multi-env-bucket-harsha-6197"]
 }
