d81e33bb665b54ac36462062495e8fbdf883bb2b
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-07-25 13:29:59 +0530

    removing multi-env-bucket-harsha-6197 in vars.tf in s3 as it was manually created

--- modules/s3-bucket/vars.tf
+++ modules/s3-bucket/vars.tf
@@ -1,6 +1,6 @@
 variable "buckets_to_create" {
   type = set(string)
-  default = ["sri6197-bucket","harsha-7916-bucket","multi-env-bucket-harsha-6197"]
+  default = ["sri6197-bucket","harsha-7916-bucket"]
 }
 variable "env" {
   
